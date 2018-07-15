157d074a81419a59257d666027da7be56c47ac89
Rendy Febry <rendyfebry@gmail.com>
2018-07-15 19:08:26 +0700

    Add promos section to homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap'
 
-import logo from '../../assets/images/logo.svg'
+const promos = [
+	{
+		title: 'USP Salestock',
+		imageUrl:
+			'https://imager-next.global.ssl.fastly.net/images/resized/480/widget_images/guest-abtest-usp-home-banner.jpg',
+		url: 'https://www.salestockindonesia.com/promosi/usp-salestock',
+	},
+	{
+		title: 'Coba Dulu Baru Bayar',
+		imageUrl:
+			'https://imager-next.global.ssl.fastly.net/images/resized/480/fb7f7004-9ee5-44e2-a4c2-f32e902389af.jpeg',
+		url: 'https://www.salestockindonesia.com/promosi/coba-dulu-baru-bayar',
+	},
+	{
+		title: 'Keuntungan Member',
+		imageUrl:
+			'https://imager-next.global.ssl.fastly.net/images/resized/480/widget_images/guest-abtest-member-benefit.jpg',
+		url: 'https://www.salestockindonesia.com/promosi/5-keuntungan-ajaib-jadi-member',
+	},
+]
 
 class Home extends Component {
+	get promoList() {
+		const PromoList = promos.map(promo => (
+			<Col xs={12} md={4} key={promo.title}>
+				<a href={promo.url}>
+					<img src={promo.imageUrl} className="img-fluid" alt={`Promo ${promo.title}`} />
+				</a>
+			</Col>
+		))
+		return PromoList
+	}
 	render() {
 		return (
 			<div className="Home">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to SaleStock</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<Container>
+					<Row>{this.promoList}</Row>
+				</Container>
 			</div>
 		)
 	}
